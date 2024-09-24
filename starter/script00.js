9e59c65f6e4bb86b2ee5d1274e717994a4ff6874
MEEX-Valentin <valentin.meex@student.hepl.Be>
2024-09-24 12:17:32 +0200

    test

--- starter/script00.js
+++ starter/script00.js
@@ -1,1 +1,21 @@
 console.log("Hello, World !");
+
+let myVar = "Bonjour";
+let myNumber = 1 + 1;
+let myBool = false;
+let myBool2 = true;
+
+myVar = 'TOTO';
+myNumber = 2;
+
+const myConst = 'bonjour';
+
+
+console.log(myVar);
+
+const titi = ""
+const myAge = prompt("Quel est ton âge ? ");
+
+
+
+console.log(myAge);
