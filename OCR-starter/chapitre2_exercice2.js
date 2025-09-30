93d3c9f54d526db352087665e45be925429f8945
MEEX-Valentin <valentin.meex@student.hepl.Be>
2025-09-30 16:24:39 +0200

    last OCR and script008 completed

--- OCR-starter/chapitre2_exercice2.js
+++ OCR-starter/chapitre2_exercice2.js
@@ -7,15 +7,16 @@
 */
 
 // Initialisez la variable celsius avec une valeur de votre choix
-
+let celsius = 23;
 
 // Calculez la température équivalente en degrés Fahrenheit et stockez-la dans la variable fahrenheit
-
+let fahrenheit = (celsius*9/5)+32;
 
 // Affichez la température en degrés Celsius et sa conversion en degrés Fahrenheit dans la console en utilisant une phrase descriptive
-
+console.log("Il fait actuellement "+celsius+" degrés celsius."+" Ce qui équivaut à "+fahrenheit+" degrés fahrenheit.")
 
 // Modifiez la valeur de la variable celsius
-
-
+celsius = 29;
+fahrenheit = (celsius*9/5)+32;
 // Répétez le calcul et l'affichage pour voir la conversion avec une autre température
+console.log("Aujourd'hui la température a augmentée, il fait actuellement "+celsius+" degrés celsius."+" Ce qui équivaut à "+fahrenheit+" degrés fahrenheit.");
--- OCR-starter/chapitre2_exercice3.js
+++ OCR-starter/chapitre2_exercice3.js
@@ -1,8 +1,16 @@
 // Permutation de variables
 
 // Solution classique utilisant une variable temporaire
+let a= 3;
+let b;
+
+b = a;
+a = b;
 
 // Solution alternative valable uniquement pour des nombres
 /*
 // Échange de nombre1 et nombre2 sans variable temporaire
 */
+let c = 16;
+let d = 9;
+[c,d] = [d,c];
