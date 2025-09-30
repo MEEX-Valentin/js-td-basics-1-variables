30d56fa7d8776251bbc31b5838b025978d726358
MEEX-Valentin <valentin.meex@student.hepl.Be>
2025-09-30 16:45:21 +0200

    script009 and script010 completed

--- starter/script010.js
+++ starter/script010.js
@@ -3,12 +3,24 @@
  */
 
 // 1. Déclarez les variables now, ageJohn, ageMark, yearJohn et yearMark, puis affectez aux trois premières les valeurs 2019, 28 et 33.
+let now = 2019;
+let ageJhon = 28;
+let ageMark = 33;
+let yearJhon;
+let yearMark;
 
 // Opérateurs mathématiques
 
 // 2. Affectez à yearJohn sa date de naissance (en la calculant à partir de l'année courante et de son âge). Faites de même pour yearMark. Ensuite, affichez la date de naissance de John et "Mark est né en …".
+yearJhon = now - ageJhon;
+yearMark = now - ageMark;
+console.log(yearJhon);
+console.log(`Mark est né en ${yearMark}`);
 
 // 3. Affichez l'année à laquelle nous serons dans deux ans, le double de l'année et le dixième de l'année.
+console.log(now + 2);
+console.log(now * 2);
+console.log(now * 0.1);
 
 // Opérateurs logiques
 
